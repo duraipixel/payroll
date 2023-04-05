0dad9d90fa2138f778b69eb5745141845daef48b
santhoshpixel <santhoshd.pixel@gmail.com>
2023-04-05 14:57:17 +0530

    other school

--- app/Http/Controllers/Master/OtherSchoolController.php
+++ app/Http/Controllers/Master/OtherSchoolController.php
@@ -72,7 +72,7 @@ class OtherSchoolController extends Controller
         $id = $request->id ?? '';
         $data = '';
         $validator = Validator::make($request->all(), [
-            'school_name' => 'required|string|unique:other_schools,name,' . $id,
+            'school_name' => 'required|string|unique:other_schools,name,' . $id .',id,deleted_at,NULL',
         ]);
         
         if ($validator->passes()) {
