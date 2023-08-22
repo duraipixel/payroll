a8cbf6997950b711232991e15730ad02a99a3184
duraibyes <duraibytes@gmail.com>
2023-08-22 18:55:10 +0530

    test one

--- app/Repositories/CronRepository.php
+++ app/Repositories/CronRepository.php
@@ -13,8 +13,8 @@ class CronRepository
     {
 
         $date = date('Y-m-d');
-        $date = '2023-07-01';
-        $end_date = '2023-07-31';
+        $date = '2023-08-01';
+        $end_date = '2023-08-10';
 
         // $url = 'http://192.168.1.46:8085/att/api/dailyAttendanceReport/';
         $url = 'http://192.168.1.46:8085/att/api/dailyAttendanceReport/?start_date=' . $date . '&end_date=' . $end_date . '&page_size=1000000';
