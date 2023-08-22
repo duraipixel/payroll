2db18a350d9ef8ba80026c01c7fa1360f4d1d937
duraibyes <duraibytes@gmail.com>
2023-08-22 17:01:22 +0530

    it

--- app/Repositories/CronRepository.php
+++ app/Repositories/CronRepository.php
@@ -25,6 +25,10 @@ class CronRepository
         if ($response->successful()) {
             $responseData = $response->json(); // Assuming the response is in JSON format
             
+            dump( gettype( $responseData ) );
+            if( isset( $responseData ) && !empty( $responseData )) {
+                dd( $responseData );
+            }
             return $responseData;
             // Do something with $responseData here
         } else {
