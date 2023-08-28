22c6b4735cbb6b241d3a6487a548065e123feb12
duraipixel <durairaj.pixel@gmail.com>
2023-08-28 11:47:05 +0530

    margin top added

--- app/Http/Controllers/Staff/StaffAppointmentDetailController.php
+++ app/Http/Controllers/Staff/StaffAppointmentDetailController.php
@@ -150,7 +150,14 @@ class StaffAppointmentDetailController extends Controller
                 $document = str_replace('$' . $key, $value, $document);
             }
 
-            $pdf = PDF::loadView('pages.masters.appointment_order_model.dynamic_pdf', ['data' => $document])->setPaper('legal', 'portrait');
+            $pdfConfig = [
+                'margin_top' => 100, 
+                
+            ];
+
+            $pdf = PDF::loadView('pages.masters.appointment_order_model.dynamic_pdf', ['data' => $document])
+                    ->setPaper('legal', 'portrait') ->setOptions($pdfConfig);
+
             $path = 'public/order_preview/'.$user_info->id;
             if (File::exists($path)) {
                 File::deleteDirectory($path);
--- resources/views/pages/masters/appointment_order_model/dynamic_pdf.blade.php
+++ resources/views/pages/masters/appointment_order_model/dynamic_pdf.blade.php
@@ -3,26 +3,36 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
-    .noborder td{
-        font-size:12px;
+    @page {
+        margin-top: 150px;
     }
-    .leave-table td, th {
+
+    .noborder td {
+        font-size: 12px;
+    }
+
+    .leave-table td,
+    th {
         padding: 5px;
-        border:1px solid black;
+        border: 1px solid black;
     }
+
     .noborder-x {
         border-left: none !important;
         border-right: none !important;
     }
+
     .noborder-s {
         border-left: none !important;
     }
+
     .noborder-e {
         border-right: none !important;
     }
 </style>
+
 <body>
-   {!! $data !!}
+    {!! $data !!}
 </body>
 
 </html>
