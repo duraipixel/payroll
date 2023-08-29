3a1093ec67603602df832e07a03df3e8d6339618
duraipixel <durairaj.pixel@gmail.com>
2023-08-29 16:31:17 +0530

    insert test in cron

--- app/Http/Controllers/TestOneController.php
+++ app/Http/Controllers/TestOneController.php
@@ -154,6 +154,54 @@ class TestOneController extends Controller
         // Check if the request was successful
         if ($response->successful()) {
             $responseData = $response->json(); // Assuming the response is in JSON format
+            if (isset($responseData['data']) && !empty($responseData['data'])) {
+                foreach ($responseData['data'] as $items) {
+                    $ins = [];
+                    $institute_code = $items['emp_code'];
+                    $user_info = User::where('institute_emp_code', $institute_code)->first();
+                    $from_time = $items['check_in'];
+                    $to_time = $items['check_out'];
+                    $total_time = $items['duration'] ?? '00:00';
+                    $clock_in = $items['clock_in'] ?? '00:00';
+                    $clock_out = $items['clock_out'] ?? '00:00';
+                    $total_clocked_time = $items['total_time'] ?? '00:00';
+                    
+                    $current_date = date('Y-m-d', strtotime($items['att_date']));
+                    $attendance_status = $items['attendance_status'];
+                   
+                    $attendance_status = strip_tags($attendance_status);
+                    $attendance_status = explode('(', $attendance_status);
+                    $ins['academic_id'] = academicYearId();
+                    $ins['employment_id'] = $user_info->id;
+                    $ins['institute_id'] = $user_info->institute_id ?? '';
+                    $ins['institute_emp_code'] = $institute_code ?? '';
+                    $ins['attendance_date'] = $current_date;
+                    $ins['reporting_manager'] = $user_info->reporting_manager_id ?? null;
+                    if( current($attendance_status) != 'Absence' &&  current($attendance_status) != 'Present') {
+                        $a_status = 'Present';
+                        $ins['other_status'] = current($attendance_status);
+                    } else {
+                        $a_status = current($attendance_status);
+                    }
+                    $ins['attendance_status'] = $a_status ?? null;
+                    $ins['reason'] = null;
+
+                    $ins['from_time'] = new \Illuminate\Database\Query\Expression("CAST('$from_time' AS TIME)");
+                    $ins['to_time'] = new \Illuminate\Database\Query\Expression("CAST('$to_time' AS TIME)");
+                    $ins['total_time'] = new \Illuminate\Database\Query\Expression("CAST('$total_time' AS TIME)");
+
+                    $ins['clock_in'] = new \Illuminate\Database\Query\Expression("CAST('$clock_in' AS TIME)");
+                    $ins['clock_out'] = new \Illuminate\Database\Query\Expression("CAST('$clock_out' AS TIME)");
+                    $ins['total_clocked_time'] = new \Illuminate\Database\Query\Expression("CAST('$total_clocked_time' AS TIME)");
+                    $ins['api_response'] = serialize($items);
+                    
+                    $check_array = ['attendance_date' => $current_date, 'employment_id' => $user_info->id];
+                  
+                    $entry_info = AttendanceManualEntry::updateOrCreate($check_array, $ins);
+                    dump( $loop->iteration );
+                }
+                
+            }
             return $responseData;
             // Do something with $responseData here
         } else {
