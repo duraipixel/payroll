bf9d6e6eee48b25881ab294cf1eb99f2a315cb46
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-05-22 12:17:11 +0530

    bank loan changes

--- app/Models/Staff/StaffELEntry.php
+++ app/Models/Staff/StaffELEntry.php
@@ -9,6 +9,6 @@ class StaffELEntry extends Model
 {
     use HasFactory;
     protected $table='staff_el_entries';
-    protected $fillable = ['staff_id','academic_id','calendar_id','from_date','to_date','leave_days','remarks','leave_mapping_id'];
+    protected $fillable = ['staff_id','academic_id','calender_id','from_date','to_date','leave_days','remarks','leave_mapping_id'];
 
 }
