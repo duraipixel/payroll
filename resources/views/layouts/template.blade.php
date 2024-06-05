7206734672840474d08b93f040934a5be6ecae5b
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-06-05 17:54:35 +0530

    calander changes

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -239,6 +239,7 @@
                     'Today': [moment(), moment()],
                     'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
                     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                     'Next 7 Days': [moment().add(6, 'days'), moment()],
                     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                     'This Month': [moment().startOf('month'), moment().endOf('month')],
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1,
