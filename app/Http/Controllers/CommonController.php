9d70d8e59fb0067397f8668a9f9ec215715acf9c
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-07-17 14:05:32 +0530

    Update CommonController.php

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -279,6 +279,7 @@ class CommonController extends Controller
                         })
                         ->InstituteBased()
                         ->where(['status' => 'active'])
+                        ->where('transfer_status' ,'active')
                         // ->Academic()                       
                         ->get();
 
