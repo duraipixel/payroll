720cba0bd666183790c749b802ce10d3e4cbc490
ajith-mv <ajith.mv@capecomsolutions.com>
2025-01-30 21:55:11 +0530

    Update PayrollImport.php

--- app/Imports/PayrollImport.php
+++ app/Imports/PayrollImport.php
@@ -39,86 +39,6 @@ class PayrollImport implements ToCollection,WithHeadingRow
       
         set_time_limit(0);
         ini_set('memory_limit', '-1');
-    //     DB::transaction(function() use ($rows
-    //     ) {
-    //     //StaffSalaryPattern::update(["is_current"=>'no']);
-
-    //   foreach($rows as $row){
-
-    //     $dateTime =  date('Y-m-d', strtotime('2024-03-01'));
-    //     $date=date('Y-m-d', strtotime('2024-03-01'));
-    //     $staff=User::where('institute_emp_code',$row["inst_emp_code"])->first();
-    //     if(isset( $staff)){
-    //     $insert_data['staff_id'] = $staff->id;
-    //     $insert_data['salary_no'] = date('ymdhis');
-    //     $insert_data['total_earnings'] =(int)$row["gross"];
-    //     $insert_data['total_deductions'] = (int)$row["ded"];
-    //     $insert_data['gross_salary'] =(int)$row["gross"];
-    //     $insert_data['net_salary'] = (int)$row["net"];
-    //     $insert_data['is_salary_processed'] = 'no';
-    //     $insert_data['status'] = 'active';
-    //     $insert_data['effective_from'] = date('Y-m-d', strtotime($date));
-    //     $insert_data['employee_remarks'] = $row["remarks"] ?? NULl;
-    //     $insert_data['remarks'] = $row["remarks"] ?? NULL;
-    //     $insert_data['payout_month'] = date('Y-m-d', strtotime($date));
-    //      $insert_data['salary_month'] =date('Y-m-d', strtotime($date));
-    //     // $insert_data['salary_year'] = date('Y', strtotime($date));
-    //     $insert_data['verification_status'] = 'approved';
-    //     $insert_data['is_current'] = 'yes';
-    //     $insert_data['institute_id']=$staff->institute_id;
-    //     $insert_data['addedBy'] = 1;
-        
-    //     $salary_info = StaffSalaryPattern::updateOrCreate(['staff_id' => $staff->id,'payout_month'=>date('Y-m-d', strtotime($date))], $insert_data);
-    //     $history_info = StaffSalaryPatternHistory::create($insert_data);
-    //     /**
-    //      * update status in it statements
-    //      */
-
-    //     $ins = [
-    //     'Basic' => $row['basic'],
-    //     'DA' => $row['da'],
-    //     'HRA' => $row['hra'],
-    //     'TA' => $row['ta'],
-    //     'PBA' => $row['pba'],
-    //     'PBADA' => $row['pbada'],
-    //     'DSA' => $row['dsa'],
-    //     'MNA' => $row['mna'],
-    //     'Arrear' => $row['arr'],
-    //     'EPF' => $row['epf'],
-    //     'ESI' => $row['esi'],
-    //     'LIC' => $row['lic'],
-    //     'Bank Loan' => $row['bank_loan'],
-    //     'Loan' => $row['ol'],
-    //     'Contribution' => $row['contributions'],
-    //     'IT' => $row['tax'],
-    //     'Other' => $row['other'],
-    //     ];
-
-      
-    //     foreach ($ins as $key=>$items_pay) {
-    //         if($key=="Arrear"){
-    //             $field=SalaryField::where('short_name','like',"%{$key}%")->where('salary_head_id',1)->first();
-    //         }else{
-    //             $field=SalaryField::where('short_name','like',"%{$key}%")->first();
-    //         }
-    //         if(isset($staff->id) && isset($field)){
-    //         $field_data['staff_id'] = $staff->id;
-    //         $field_data['staff_salary_pattern_id'] = $salary_info->id;
-    //         $field_data['field_id'] = $field->id;
-    //         $field_data['field_name'] = $field->name;
-    //         $field_data['amount'] =(int)$items_pay;
-    //         $field_data['percentage'] =0;
-    //         $field_data['reference_type'] = ($field->salary_head_id==1)? 'EARNINGS': 'DEDUCTIONS';
-    //         $field_data['reference_id'] =$field->salary_head_id;
-    //          StaffSalaryPatternField::updateOrCreate(['staff_id' => $staff->id,'staff_salary_pattern_id'=> $salary_info->id,'field_name'=>$field->name],$field_data);
-    //         $field_data['staff_salary_pattern_id'] = $history_info->id;
-    //         StaffSalaryPatternFieldHistory::create($field_data);
-    //         }
-    //      }
-        
-    //     }
-    //   }
-    //  });
        $dateTime =  date('Y-m-d', strtotime('2024-12-01'));
        $date= date('Y-m-d', strtotime('2024-12-01'));
       $payroll_date = $date;
@@ -545,9 +465,11 @@ class PayrollImport implements ToCollection,WithHeadingRow
                       }
                     }
                 });
+                DB::commit();
                 }
               
                 catch (\Throwable $e) {
+                    DB::rollBack();
                   dd($e);
                 }
                 return true;
